64eda0a838499524a8cfe0b3af3c93bf869f4201
Adrian Mitre <mitrehealthcare@gmail.com>
2024-04-27 14:43:34 -0700

    app test adjusmet

--- src/App.test.js
+++ src/App.test.js
@@ -3,11 +3,11 @@ import App from "./App";
 import renderer from "react-test-renderer";
 
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 test("renders a snapshot", () => {
   const tree = renderer.create(<App />)
